cbc157563fa9288abd32949240ef8d96a33bf090
Evgeniy Zabolotniy <eugenezabolotniy@yandex.ru>
2023-05-09 19:15:03 +0800

    Multiline comments removal was implemented

--- Sources/RemoveComments/String+RemoveComments.swift
+++ Sources/RemoveComments/String+RemoveComments.swift
@@ -6,19 +6,105 @@
 //
 
 extension String {
-  public func removeComments() -> String {
-    var commentStartIndex = endIndex
-    for i in indices.dropFirst() {
-      if self[i] == "/" && self[index(before: i)] == "/" {
-        commentStartIndex = index(before: i)
-        break
+  public func removeCommentsAndTrimWhitespaces() -> String {
+    var isInsideMultilineComment = false
+    return split(whereSeparator: { $0.isNewline }).map {
+      String($0.removeCommentsAndTrimWhitespaces(&isInsideMultilineComment))
+    }.filter { !$0.isEmpty }
+      .joined(separator: "\n")
+  }
+}
+
+extension StringProtocol {
+  fileprivate func removeCommentsAndTrimWhitespaces(_ isInsideMultilineComment: inout Bool) -> any StringProtocol {
+    guard first(where: { $0.isNewline }) == nil else {
+      fatalError("This function should be called on a single line string")
+    }
+    return removeMultilineComment(&isInsideMultilineComment)
+      .removeDoubleSlashComment()
+      .trimWhitespaces()
+  }
+}
+
+// MARK: - Multiline comment removal
+extension StringProtocol {
+  private func removeMultilineComment(_ isInsideMultilineComment: inout Bool) -> any StringProtocol {
+    let commentStartIndex = startIndexOfOpenMultilineComment
+    let commentEndIndex = endIndexOfClosedMultilineComment
+    if isInsideMultilineComment, commentEndIndex == nil {
+      return ""
+    }
+    if let commentStartIndex = commentStartIndex, commentEndIndex == nil {
+      isInsideMultilineComment = true
+      return self[..<commentStartIndex]
+    }
+    if commentStartIndex == nil, let commentEndIndex = commentEndIndex {
+      isInsideMultilineComment = false
+      return self[commentEndIndex...]
+    }
+    if let commentStartIndex = commentStartIndex, let commentEndIndex = commentEndIndex {
+      if commentStartIndex < commentEndIndex {
+        var tmp = String(self)
+        tmp.replaceSubrange(commentStartIndex..<commentEndIndex, with: "")
+        return tmp
+      } else {
+        return self[commentEndIndex..<commentStartIndex]
       }
     }
+    return self
+  }
 
-    let noCommentString = self[..<commentStartIndex]
-    if let firstLetterIndex = noCommentString.firstIndex(where: { !$0.isWhitespace }),
-       let lastLetterIndex = noCommentString.lastIndex(where: { !$0.isWhitespace }) {
-      return String(self[firstLetterIndex...lastLetterIndex])
+  public var startIndexOfOpenMultilineComment: String.Index? {
+    for i in indices.dropFirst() {
+      let prevIndex = index(before: i)
+      if self[prevIndex] == "/" && self[i] == "*" {
+        return prevIndex
+      }
+    }
+    return nil
+  }
+
+  public var endIndexOfClosedMultilineComment: String.Index? {
+    for i in indices.dropFirst() {
+      let prevIndex = index(before: i)
+      if self[prevIndex] == "*" && self[i] == "/" {
+        return index(after: i)
+      }
+    }
+    return nil
+  }
+}
+
+// MARK: - Double slash comment removal
+extension StringProtocol {
+  private func removeDoubleSlashComment() -> any StringProtocol {
+    guard first(where: { $0.isNewline }) == nil else {
+      fatalError("This function should be called on a single line string")
+    }
+    let commentStartIndex = startIndexOfDoubleSlash ?? endIndex
+    return self[..<commentStartIndex]
+  }
+
+  private var startIndexOfDoubleSlash: String.Index? {
+    for i in indices.dropFirst() {
+      let prevIndex = index(before: i)
+      if self[prevIndex] == "/" && self[i] == "/" {
+        return prevIndex
+      }
+    }
+    return nil
+  }
+}
+
+// MARK: - Trimming whitespaces
+extension StringProtocol {
+  private func trimWhitespaces() -> any StringProtocol {
+    guard first(where: { $0.isNewline }) == nil else {
+      fatalError("This function should be called on a single line string")
+    }
+    if let firstLetterIndex = firstIndex(where: { !$0.isWhitespace }),
+       let lastLetterIndex = lastIndex(where: { !$0.isWhitespace }) {
+      return self[firstLetterIndex...lastLetterIndex]
     }
     return ""
   }
--- Tests/RemoveCommentsTests/String+RemoveCommentsTests.swift
+++ Tests/RemoveCommentsTests/String+RemoveCommentsTests.swift
@@ -11,43 +11,128 @@ import XCTest
 final class StringRemoveCommentsTests: XCTestCase {
   func testEmptyLine() throws {
     let line = ""
-    let removedComment = line.removeComments()
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
     XCTAssertEqual(removedComment, "")
   }
 
   func testNewLine() throws {
     let line = "\n"
-    let removedComment = line.removeComments()
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
     XCTAssertEqual(removedComment, "")
   }
 
   func testWhitespacesAndNewLine() throws {
     let line = "     \n"
-    let removedComment = line.removeComments()
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
     XCTAssertEqual(removedComment, "")
   }
 
   func testCommentLine() throws {
     let line = "// Comment line"
-    let removedComment = line.removeComments()
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
     XCTAssertEqual(removedComment, "")
   }
 
   func testCommentLineWithSpacePrefix() throws {
     let line = "   // Comment line"
-    let removedComment = line.removeComments()
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
     XCTAssertEqual(removedComment, "")
   }
 
   func testCodeWithCommentLine() throws {
     let line = "push local 1 // Comment line"
-    let removedComment = line.removeComments()
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
     XCTAssertEqual(removedComment, "push local 1")
+  }
+
+  func testOpenClosedCommentLine() throws {
+    let line = "/* Comment line */"
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "")
+  }
+
+  func testOpenClosedCommentLineWithSpacePrefix() throws {
+    let line = "   /* Comment line*/"
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "")
+  }
+
+  func testLeadingCodeWithOpenClosedCommentLine() throws {
+    let line = "push local 1 /* Comment line    */"
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "push local 1")
+  }
+
+  func testTrailingCodeWithOpenClosedCommentLine() throws {
+    let line = " /* Comment line    */ push local 1"
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "push local 1")
+  }
+
+  func testCodeWithOpenClosedCommentLine() throws {
+    let line = " push /* Comment line    */ local 1"
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "push  local 1")
+  }
+
+  func testCodeWithOpenMultilineComment() throws {
+    let line = " push local 1 /* Comment line "
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "push local 1")
+  }
+
+  func testCodeWithClosedMultilineComment() throws {
+    let line = " Comment line */ push local 1"
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "push local 1")
+  }
+
+  func testCodeWithClosedOpenMultilineComment() throws {
+    let line = " Comment line    */ push local 1 /* Comment line "
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "push local 1")
+  }
+
+  func testCodeWithMultilineComment() throws {
+    let line = """
+    /*
+      Multi
+      Line
+      Comment
+    */
+    push local 1
+    /* one more
+    multi comment */
+
+    """
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(removedComment, "push local 1")
+  }
+
+  func testCodeWithMultilineComment1() throws {
+    let line = """
+    /*
+      Multi
+      Line
+      Comment
+    */ push local 1 /* one more
+    multi comment */
+    push local 2
+
+    """
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
+    XCTAssertEqual(
+      removedComment,
+      """
+      push local 1
+      push local 2
+      """
+    )
   }
 
   func testNoCommentLine() throws {
     let line = "push local 1"
-    let removedComment = line.removeComments()
+    let removedComment = line.removeCommentsAndTrimWhitespaces()
     XCTAssertEqual(removedComment, line)
   }
 }
